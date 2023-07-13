013d401bd4f166f28607c037dea7955559bff7eb
ned-park <87483870+ned-park@users.noreply.github.com>
2023-07-13 14:40:16 -0400

    feat: add fontclamp for WeatherTable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        tableclamp: "clamp(0.75rem, 2vw, 1.25rem)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
