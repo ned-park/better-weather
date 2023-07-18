8d31736a62244ab0fd9ac0c81917a95540479346
ned-park <87483870+ned-park@users.noreply.github.com>
2023-07-18 13:38:23 -0400

    fix: bug during filter

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,6 +16,7 @@ function Modal({
 
   const handleSelection = (e: MouseEvent, place: Place) => {
     e.preventDefault();
+    e.stopPropagation();
     setLatLong({
       latitude: String(place.latitude),
       longitude: String(place.longitude),
@@ -24,16 +25,16 @@ function Modal({
     setShowModal(false);
   }
   return (
-    <section className="bg-sky-100 bg-opacity-80 fixed top-0 left-0 w-full h-full flex justify-center items-center">
-      <div className="bg-sky-50 bg-opacity-100 shadow-lg p-16 overflow-visible overflow-auto">
-        <ul className="flex flex-col gap-4 text-center shadow-lg p-16 overflow-visible overflow-auto">
+        <section className="fixed bg-sky-100 bg-opacity-80 top-0 left-0 w-full h-full flex justify-center items-center z-10">
+    <div className="bg-sky-50 bg-opacity-100 shadow-lg p-2 md:p-16 h-200 z-50">
+        <ul className="flex flex-col gap-1 md:gap-4 text-center overflow-visible overflow-auto z-50">
           {places.map((place: Place) => (
             <li
               key={uniqId.default()}
               onClick={(e) => handleSelection(e, place)}
-              className="cursor-pointer border-b-2 grey9"
+              className="cursor-pointer border-b-2 grey"
             >
-              {[place.name, place.admin1, place.country].filter((p: string) => p.length > 0).join(", ")}
+              {[place.name, place.admin1, place.country].filter((p: string) => p && p.length > 0).join(", ")}
             </li>))}
         </ul>
       </div>
