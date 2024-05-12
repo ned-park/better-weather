3cf42ba0bfbb79975587fb645fc0d54f8b0782a7
ned-park <87483870+ned-park@users.noreply.github.com>
2024-05-12 16:31:14 -0400

    build: configure for building

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+//await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -28,6 +28,7 @@ const config = {
   },
   
   swcMinify: true,
+  output: 'standalone',
 };
 
 export default config;
